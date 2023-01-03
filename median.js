370e886ad215de410940d721b1d04cdb9e4d166b
muhammadfirmansyah1 <79295838+muhammadfirmansyah1@users.noreply.github.com>
2023-01-03 20:37:11 +0700

    Komen

--- median.js
+++ median.js
@@ -1,9 +1,9 @@
 const input = [8, 7, 7, 9, 5, 4, 2, 9];
 
 function result(input) {
-  const mid = Math.floor(input.length / 2),   //Mencari Index tengah
-    nums = [...input].sort((a, b) => a - b);
-  return input.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  const mid = Math.floor(input.length / 2),   //Mencari Index tengah dalam array
+    nums = [...input].sort((a, b) => a - b);  //sorting array
+  return input.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;  //mencari median dengan 2 kondisi, panjang array genap atau ganjil
 }
 
 console.log(result(input));
